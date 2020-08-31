88faf43f6ffa88bd160b0046cd8b21e191e4c89f
jericho <echocastro18@gmail.com>
2020-08-31 18:42:11 +0800

    Refactoring to common crud interface. closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VerService.java
@@ -5,8 +5,6 @@ import guru.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VerService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VerService extends CrudService<Vet, Long>{
+
 }
