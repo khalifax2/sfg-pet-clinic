567c75b9d0eab7e50a08fcb4f1aac673688f8cef
jericho <echocastro18@gmail.com>
2020-10-04 20:31:18 +0800

    Implemented Jpa visit Service, added loader data. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService extends AbstractJpaService<Visit, VisitRepository> implements VisitService {
+
+    public VisitSDJpaService(VisitRepository repository) {
+        super(repository);
+    }
+
+}
