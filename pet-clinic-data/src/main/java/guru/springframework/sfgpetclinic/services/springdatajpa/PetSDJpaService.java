5fee7d3b205a43f5ecaba604956e35c71f2a26a6
jericho <echocastro18@gmail.com>
2020-12-07 22:35:58 +0800

    Fixed implement PetService BUG

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -2,12 +2,13 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("springdatajpa")
-public class PetSDJpaService extends AbstractJpaService<Pet, PetRepository> {
+public class PetSDJpaService extends AbstractJpaService<Pet, PetRepository> implements PetService {
 
     public PetSDJpaService(PetRepository repository) {
         super(repository);
